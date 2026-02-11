4043ddf1f69a28485536d3f43e047d495459388c
AI-test-tech <sherow1982@gmail.com>
2026-02-11 15:34:59 +0200

    2

--- next.config.js
+++ next.config.js
@@ -7,9 +7,7 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  output: 'export',
   compress: true,
-  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
