e7a6d74ac93ba1af094fd38464175016043e4053
AI-test-tech <sherow1982@gmail.com>
2026-02-12 10:14:59 +0200

    Add Facebook page section before footer

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -36,17 +36,19 @@ export default function App({ Component, pageProps }) {
               </a>
             </div>
             
-            <div className="bg-white rounded-2xl shadow-2xl overflow-hidden">
-              <iframe 
-                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Farabads.me%2F&tabs=timeline&width=500&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" 
-                width="100%" 
-                height="600" 
-                style={{ border: 'none', overflow: 'hidden' }} 
-                scrolling="no" 
-                frameBorder="0" 
-                allowFullScreen={true} 
-                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-              />
+            <div className="flex justify-center">
+              <div className="bg-white rounded-2xl shadow-2xl overflow-hidden">
+                <iframe 
+                  src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Farabads.me%2F&tabs=timeline&width=500&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" 
+                  width="500" 
+                  height="600" 
+                  style={{ border: 'none', overflow: 'hidden' }} 
+                  scrolling="no" 
+                  frameBorder="0" 
+                  allowFullScreen={true} 
+                  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                />
+              </div>
             </div>
           </div>
         </section>
