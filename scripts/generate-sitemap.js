277aef8ca3db0068a393dd73bd5b20f0fc2a6e24
AI-test-tech <sherow1982@gmail.com>
2026-02-13 12:42:23 +0200

    3

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -5,52 +5,34 @@ const baseUrl = 'https://omany.storesads.shop';
 const now = new Date().toISOString();
 
 let xml = `<?xml version="1.0" encoding="UTF-8"?>
-
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url>
 <loc>${baseUrl}/</loc>
-
 <lastmod>${now}</lastmod>
-
 <changefreq>weekly</changefreq>
-
-<priority>1</priority>
-
+<priority>1.0</priority>
 </url>
 <url>
-
 <loc>${baseUrl}/shop</loc>
-
 <lastmod>${now}</lastmod>
-
 <changefreq>weekly</changefreq>
-
-<priority>1</priority>
-
+<priority>0.9</priority>
 </url>
-
 <url>
 <loc>${baseUrl}/contact-us</loc>
-
 <lastmod>${now}</lastmod>
-<changefreq>weekly</changefreq>
-
-<priority>1</priority>
+<changefreq>monthly</changefreq>
+<priority>0.5</priority>
 </url>
-
 `;
 
 data.forEach(p => {
   xml += `<url>
-
 <loc>${baseUrl}/product/${p.id}</loc>
-
 <lastmod>${now}</lastmod>
 <changefreq>daily</changefreq>
 <priority>0.8</priority>
-
 </url>
-
 `;
 });
 
