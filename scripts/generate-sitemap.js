2540db71e809f29d4faf32be743e9293612b1acf
AI-test-tech <sherow1982@gmail.com>
2026-02-13 13:38:00 +0200

    node scripts\generate-sitemap.js

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -7,7 +7,7 @@ const now = new Date().toISOString();
 let xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url>
-<loc>${baseUrl}/</loc>
+<loc>${baseUrl}</loc>
 <lastmod>${now}</lastmod>
 <changefreq>weekly</changefreq>
 <priority>1.0</priority>
