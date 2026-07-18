05ed82b133581dd307222baaffdd7c83fea943ee
AI-test-tech <sherow1982@gmail.com>
2026-07-18 19:57:05 +0200

    feat: add GTM-NP6JHSNR tag

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -64,6 +64,22 @@ export default function Document() {
             `,
           }}
         />
+
+        {/* GTM-NP6JHSNR - deferred */}
+        <script
+          defer
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.addEventListener('load', function() {
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-NP6JHSNR');
+              });
+            `,
+          }}
+        />
       </Head>
       <body>
         <noscript>
